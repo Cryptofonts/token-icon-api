3f6c51aba383e1bf71e520df4e4e25e28986ec61
monzanifabio <renderbros@gmail.com>
2023-07-28 00:10:00 +0100

    Removed ping endpoint

--- index.js
+++ index.js
@@ -14,11 +14,6 @@ const headers = {
 
 // Base endpoint
 app.get("/", (req, res) => {
-  res.json("Hellooo!");
-});
-
-// Simple ping endpoint
-app.get("/ping", (req, res) => {
   res.json("Cryptofonts to the moon!");
 });
 
